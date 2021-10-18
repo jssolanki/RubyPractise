6a624eec5db3484beb7fb518957e5f13c043b3f6
Jitendra Solanki <jitendra.solanki@greatlearning.in>
2021-10-18 11:50:23 +0530

    question 3 multiple option

--- question3.rb
+++ question3.rb
@@ -2,6 +2,12 @@ def double(array)
     array.each_with_index { |val,idx| array[idx] = array[idx]*2 }
 end
 
+def doublev1(array)
+    array.map{ |num| num*2 }
+end
+
 array = [1,2,3]
 double(array)
 puts array
+
+puts doublev1(array)
