0ab89fa1573fd13547becbab868564b25c09b7f6
Jitendra Singh <jssolanki4u@yahoo.in>
2021-10-19 22:22:02 +0530

    Update student.rb

--- student.rb
+++ student.rb
@@ -1,10 +1,15 @@
+# frozen_string_literal: true
+
+# this class is about student
 class Student
-    attr_accessor :name
-    attr_writer :grade
-    def initialize(name)
-        self.name = name
-    end
-    def result
-        puts "Well done!" if joe.better_grade_than?(bob)
-    end
+  attr_accessor :name
+  attr_writer :grade
+
+  def initialize(name)
+    self.name = name
+  end
+
+  def result
+    puts 'Well done!' if joe.better_grade_than?(bob)
+  end
 end
